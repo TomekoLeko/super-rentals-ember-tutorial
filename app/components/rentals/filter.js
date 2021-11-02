e86f609d8b254550c55110b787a80618408d6fd6
TomekoLeko <ays@vp.pl>
2021-11-02 13:25:35 +0100

    Filtering by city, type, category

--- app/components/rentals/filter.js
+++ app/components/rentals/filter.js
@@ -5,9 +5,14 @@ export default class RentalsFilterComponent extends Component {
     let { rentals, query } = this.args;
 
     if (query) {
-      rentals = rentals.filter((rental) => rental.title.includes(query));
-    }
-
+      rentals = rentals.filter((rental) =>
+      {
+       return rental.title.toLowerCase().includes(query.toLowerCase())
+              || rental.city.toLowerCase().includes(query.toLowerCase())
+              || rental.category.toLowerCase().includes(query.toLowerCase())
+              || rental.type.toLowerCase().includes(query.toLowerCase())
+      })
+    };
     return rentals;
   }
 }
